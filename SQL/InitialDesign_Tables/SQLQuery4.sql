e96e2dfb6e309cbc51e70587d7daf535726d8184
atuldc <atul.das13@gmail.com>
2024-03-22 22:54:54 +0530

    Intial DB Design Discussion

---
+++ SQL/InitialDesign_Tables/SQLQuery4.sql
@@ -0,0 +1,9 @@
+INSERT INTO Miscellaneous.Type (TypeGUID, TypeKey, TypeName, TypeDescription, CreatedDate, LastUpdate) 
+VALUES (NEWID(), 5, 'BillingAddress', 'Company Billing Address', GETDATE(), GETDATE())
+
+
+
+
+
+
+
